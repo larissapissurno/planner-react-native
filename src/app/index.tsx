f981e117ffe6609921be1bc2a66b3ec36c328b89
Larissa Pissurno <larissapissurno@gmail.com>
2024-07-13 15:40:28 -0300

    Refactor index.tsx to include new components and styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,46 @@
-import { Text, View } from 'react-native'
+import { Input } from '@/components/input'
+import { Image, Text, View } from 'react-native'
+
+import { colors } from '@/styles/colors'
+
+import {
+  MapPin as MapPinIcon,
+  Calendar as CalendarIcon,
+  Settings2 as SettingsIcon
+} from 'lucide-react-native'
+import { Button } from './button'
+
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className='text-zinc-400'>Initial structure / index</Text>
+    <View className='flex-1 items-center justify-center px-5'>
+      <Image
+        source={require("@/assets/logo.png")}
+        className='h-8'
+        resizeMode='contain'
+      />
+
+      <Text className='text-zinc-400 font-regular text-center text-lg mt-3'>
+        Convide seus amigos e planeje sua{"\n"}próxima viagem
+      </Text>
+
+      <View className='w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800'>
+        <Input>
+          <MapPinIcon color={colors.zinc[400]} size={20} />
+          <Input.Field placeholder='Para onde?' />
+        </Input>
+
+        <Input>
+          <CalendarIcon color={colors.zinc[400]} size={20} />
+          <Input.Field placeholder='Quando?' />
+        </Input>
+
+        <View className='border-b py-3 border-zinc-800'>
+          <Button variant='secondary'>
+            <Button.Title>Alterar local/data</Button.Title>
+            <SettingsIcon color={colors.zinc[200]} size={20} />
+          </Button>
+        </View>
+      </View>
     </View>
   )
 }
